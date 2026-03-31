c15b9ad43e3dc80df89dc1ba010c243b3a6e3a6a
Travon <tchk7@umkc.edu>
2026-03-31 09:49:43 -0500

    Updated to better debug database.

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -3,3 +3,23 @@ from app.utils.time import Time
 
 class BaseModel(Base, Time):
     __abstract__ = True
+
+    def __repr__(self):
+
+        class_name = self.__class__.__name__
+
+        try:
+            columns = {
+                c.name: getattr(self, c.name)
+                for c in self.__table__.columns.values()
+                if "password" not in c.name and "hash" not in c.name
+            }
+        except Exception:
+
+            columns = {
+                key: value for key, value in self.__dict__.items()
+                if not key.startswith("_") and "password" not in key and "hash" not in key
+            }
+
+        params = ", ".join(f"{key}={value}" for key, value in columns.items())
+        return f"{class_name}({params})"
